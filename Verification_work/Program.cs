b83e9b3516f4e211748084ede0075e405abc1c49
Dmitriy Linov <Dimalinov@mail.ru>
2023-02-14 09:17:51 +0300

    Создал метод проверки длинны элемента массива

--- Verification_work/Program.cs
+++ Verification_work/Program.cs
@@ -2,3 +2,10 @@
 string[] myArray2 = { "knowledge", "is", "priceless", "baggage", ":=)" };
 string[] myArray3 = { "patience", "and", "a", "little", "effort" };
 
+bool CharacterCountCheck(string element)
+{
+    char[] num = element.ToCharArray();
+    if (num.Length > 3) return false;
+    return true;
+}
+
