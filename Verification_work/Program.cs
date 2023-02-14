b64269b728ae5c00bbbec32b615218cc35ca981b
Dmitriy Linov <Dimalinov@mail.ru>
2023-02-14 08:54:17 +0300

    Объявил три массива

--- Verification_work/Program.cs
+++ Verification_work/Program.cs
@@ -1,1 +1,4 @@
-﻿
+﻿string[] myArray1 = { "the", "road", "will", "be", "mastered", "by", "walking" };
+string[] myArray2 = { "knowledge", "is", "priceless", "baggage", ":=)" };
+string[] myArray3 = { "patience", "and", "a", "little", "effort" };
+
